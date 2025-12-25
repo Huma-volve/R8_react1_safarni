ecb5446d147f0467b417be90a5cfbab6d7c68657
MahmoudFawzyHelmy <mahmoudfawzy.helmy@gmail.com>
2025-12-25 21:56:55 +0200

    upades tailwind

--- safarni/vite.config.ts
+++ safarni/vite.config.ts
@@ -1,7 +1,7 @@
-import { tailwindcss } from '@tailwindcss/vite';
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
