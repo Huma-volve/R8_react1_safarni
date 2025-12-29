8a550d92e8d0de5c9c944fb2e0e5b17a0a77ca6c
Reda Salem <redasalem287@gmail.com>
2025-12-29 22:19:46 +0200

    transform un commit commponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-// import Favorite from "@/pages/Favorite/Favorite"
+ import Favorite from "@/pages/Favorite/Favorite"
 
-import ForgetPassword from "./pages/auth/ForgetPassword"
+ import ForgetPassword from "./pages/auth/ForgetPassword"
 
-// import SignUp from "./pages/auth/SignUp"
+import SignUp from "./pages/auth/SignUp"
 
-// import Login from "./pages/auth/Login"
+ import Login from "./pages/auth/Login"
 
-// import GetStarted from "./pages/auth/GetStarted"
-// import Favorite from "./pages/Favorite/Favorite"
+import GetStarted from "./pages/auth/GetStarted"
 
-// import CarsBooking from "@/pages/CarBooking/CarsBooking"
+
+import CarsBooking from "@/pages/CarBooking/CarsBooking"
 function App() {
 
   return (
     <>
 
-    {/* <Login/> */}
-    {/* <SignUp/> */}
+     <Login/> 
+     <SignUp/> 
 
-    {/* <GetStarted/> */}
-      {/* <CarsBooking/> */}
-      {/* <Favorite/> */}
-      <ForgetPassword/>
+    <GetStarted/>
+      <CarsBooking/>
+      <Favorite/> 
+     <ForgetPassword/>
     </>
   )
 }
