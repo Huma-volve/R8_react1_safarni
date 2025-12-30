7208bfad4dc6430c14828ae970601f972c50b875
Marioo-Ayman <96667299+Marioo-Ayman@users.noreply.github.com>
2025-12-30 20:04:25 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import Favorite from "@/pages/Favorite/Favorite"
+import CarDetails from "@/pages/CarBooking/CarDetails"
+import {  Route, Routes } from "react-router-dom";
+import Profile from './pages/profile';
+import CarsBooking from "@/pages/CarBooking/CarsBooking"
+// import ForgetPassword from "./pages/auth/ForgetPassword";
+// import SignUp from "./pages/auth/SignUp";
+// import Login from "./pages/auth/Login";
+// import GetStarted from "./pages/auth/GetStarted";
+// import Otp from "./pages/auth/Otp";
+// import NewPassword from "./pages/auth/Newpassword";
+// import Done from "./pages/auth/Done";
+
+function App() {
+  return (
+    <>
+      <Routes >
+        <Route path="/cars" element={<CarsBooking/>} />
+        <Route path="/car-details" element={<CarDetails/>} />
+        <Route path="/favorite" element={<Favorite/>} />
+        <Route path="/profile/*" element={<Profile />} />       
+        {/* <Route path="/" element={<Navigate to="/GetStarted" replace />} />
+        <Route path="/GetStarted" element={<GetStarted />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgetPassword />} />
+        <Route path="/otp" element={<Otp />} />
+        <Route path="/newpassword" element={<NewPassword />} />
+        <Route path="/done" element={<Done />} />
+        <Route path="/done" element={<FlightBooking />} />
+        <Route path="/done" element={<Map />} /> */}
+      </Routes>
+    </>
+  )
+}
+
+export default App;
