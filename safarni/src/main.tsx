24358f954c1cc495eacd7e97c0fc52ad35052bfd
Marioo-Ayman <96667299+Marioo-Ayman@users.noreply.github.com>
2025-12-30 18:22:18 +0200

    update main

--- safarni/src/main.tsx
+++ safarni/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
