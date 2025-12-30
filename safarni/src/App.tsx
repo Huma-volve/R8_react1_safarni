742ce728b11ac0b64b7ac895d7e4ca7263586826
Marioo-Ayman <96667299+Marioo-Ayman@users.noreply.github.com>
2025-12-30 17:39:07 +0200

    feat: update routing in App component, add new dependencies for routing and charts, and enhance global styles

--- safarni/src/App.tsx
+++ safarni/src/App.tsx
@@ -1,10 +1,17 @@
-// import Favorite from "@/pages/Favorite/Favorite"
+import Favorite from "@/pages/Favorite/Favorite"
 import CarsBooking from "@/pages/CarBooking/CarsBooking"
+import CarDetails from "@/pages/CarBooking/CarDetails"
+import {  Route, Routes } from "react-router-dom";
+
 function App() {
 
   return (
     <>
-      <CarsBooking/>
+      <Routes >
+        <Route path="/cars" element={<CarsBooking/>} />
+        <Route path="/car-details" element={<CarDetails/>} />
+        <Route path="/favorite" element={<Favorite/>} />
+      </Routes>
     </>
   )
 }
