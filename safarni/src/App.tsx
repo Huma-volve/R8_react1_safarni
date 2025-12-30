c7a0651ee3a8f4928fad7b42e9f182080b8d58f3
wafaa <wafaahamdy732@gmail.com>
2025-12-30 18:28:11 +0200

    'updateIndex.css'

--- safarni/src/App.tsx
+++ safarni/src/App.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Profile from './pages/profile';
-// import Favorite from "@/pages/Favorite/Favorite"
+ import Favorite from "@/pages/Favorite/Favorite"
 import CarsBooking from "@/pages/CarBooking/CarsBooking"
 
 function App() {
   return (
     <Routes>
       <Route path="/profile/*" element={<Profile />} />
+      <Route path="/car-booking/*" element={<CarsBooking />} />
+      <Route path="/favorite/*" element={<Favorite />} />
+      
     </Routes>
   )
 }
