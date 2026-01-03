09259b554a40b67862ef1e186167a90010fc81c5
Reda Salem <redasalem287@gmail.com>
2026-01-03 19:30:18 +0200

    add changes in usecallback google auth

--- safarni/src/services/get.ts
+++ safarni/src/services/get.ts
@@ -9,3 +9,8 @@ export const getGoogleAuthUrl = async (): Promise<GoogleAuthResponse> => {
   const response = await api.get("/auth/google");
   return response.data;
 };
+
+export const googleAuthCallback = async (code: string) => {
+  const response = await api.get(`/auth/google/callback?code=${code}`);
+  return response.data;
+};
